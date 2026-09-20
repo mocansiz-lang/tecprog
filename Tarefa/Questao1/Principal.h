4cb5edf19f27649166235da2ada8b1f8d51ee0cc
mocansiz-lang <mocansiz@gmail.com>
2026-09-20 08:04:39 -0300

    Add files via upload

---
+++ Tarefa/Questao1/Principal.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Horario.h"
+
+class Principal
+{
+private:
+    Horario entrada;
+    Horario saida;
+
+public:
+    Principal();
+    ~Principal();
+
+    void Executar();
+    void PerguntaHorario();
+    float CalculaCusto();
+};
